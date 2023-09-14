21073e3b12d77bb88b56d163b55f254eecbbb028
danielh13210 <danielh13210@gmail.com>
2023-09-14 17:48:43 +0800

    Update autotest-validate.c

--- examples/autotest-validate/autotest-validate.c
+++ examples/autotest-validate/autotest-validate.c
@@ -3,6 +3,10 @@
 */
 
 #include "autotest-validate.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "unistd.h"
 #include <stdbool.h>
 
 /**
@@ -25,7 +29,18 @@ bool this_function_returns_false()
  * @return a string which contains the username you use for
  * git submissions.  This string should match the string in conf/username.txt
  */
+ char username[90]; // just enough to hold git usernames
+ typedef int file;
 const char *my_username()
 {
-    return "todo-please-enter-your-username-here-in-my_username";
+    // just read the file.
+    file f=open("conf/username.txt",O_RDONLY);
+    char c=0;
+	int remaining=1;
+    int count=0;
+	do {
+        remaining=read(f,&c,1);
+        if(remaining)username[count++]=c;
+	} while(remaining);
+    return username;
 }
